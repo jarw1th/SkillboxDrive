a2223386a75ab1cfcd3211cab2e1f9e71b280ec4
jarw1th <parastaev31@gmail.com>
2024-02-28 09:00:14 +0300

    initial

---
+++ ydisk_skillbox/ydisk_skillbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ydisk_skillbox
+//
+//  Created by Руслан Парастаев on 28.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
